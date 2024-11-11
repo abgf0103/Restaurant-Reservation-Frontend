54d384b3ad821c9f193ac586bf87400cc9558803
Jong1 <abgf0103@gmail.com>
2024-11-11 12:27:26 +0900

    fix : storeList

--- src/components/StoreList.js
+++ src/components/StoreList.js
@@ -3,32 +3,31 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const StoreList = () => {
-    // const [data, setData] = useState([]);
+    const [storeData, setStoreData] = useState([]);
 
-    // const getData = () => {
-    //     instance.get("/api/store/list").then((res) => {
-    //         console.log(res.data);
-    //         setData(res.data);
-    //     });
-    // };
-
-    // useEffect(() => {
-    //     getData();
-    // }, []);
+    const getData = () => {
+        instance.get("/store/list").then((res) => {
+            console.log(res.data);
+            setStoreData(res.data);
+        });
+    };
+    useEffect(() => {
+        getData();
+    }, []);
 
     return (
         <div>
-            {/* <h4>==========가게 정보 리스트==========</h4>
+            <h4>==========가게 정보 리스트==========</h4>
             <ul>
-                {data.map((item) => {
-                    // return (
-                    //     <li key={item.id}>
-                    //         <Link to={`/board/view/${item.id}`}>{item.title}</Link>
-                    //     </li>
-                    // );
+                {storeData.map((item) => {
+                    return (
+                        <li key={item.storeId}>
+                            <Link to={`/board/view/${item.id}`}>{item.storeName}</Link>
+                        </li>
+                    );
                 })}
             </ul>
-            <h4>===============================</h4> */}
+            <h4>===============================</h4>
         </div>
     );
 };
