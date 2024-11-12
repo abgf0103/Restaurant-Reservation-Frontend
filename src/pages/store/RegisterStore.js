7cbb26999cdafdc07b48c2b4846ed7e01210bcad
Jong1 <abgf0103@gmail.com>
2024-11-12 16:37:00 +0900

    create : store register

--- src/pages/store/RegisterStore.js
+++ src/pages/store/RegisterStore.js
@@ -48,54 +48,54 @@ const RegisterStore = () => {
         <div>
             <h2>가게 등록 페이지</h2>
             <Form>
-      <Form.Group className="mb-3">
-        <Form.Label>가게 이름</Form.Label>
-        <Form.Control type="email" placeholder="가게 이름을 입력하세요" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
+                <Form.Group className="mb-3">
+                    <Form.Label>가게 이름</Form.Label>
+                    <Form.Control type="email" placeholder="가게 이름을 입력하세요" />
+                    <Form.Text className="text-muted">
+                    We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
 
-      <Form.Group className="mb-3">
-        <Form.Label>카테고리를 선택하세요</Form.Label>
-        <Form.Select>
-            {categoryData.map((item) => {
-                return (
-                    // categoryData에 저장된 DB에서 가져온 카테고리를 select option에 하나씩 추가
-                    <option key={item.categoryId} value={item.categoryId}>{item.categoryTitle}</option>
-                );
-            })}
-        </Form.Select>
-      </Form.Group>
+                <Form.Group className="mb-3">
+                    <Form.Label>카테고리를 선택하세요</Form.Label>
+                    <Form.Select>
+                        {categoryData.map((item) => {
+                            return (
+                                // categoryData에 저장된 DB에서 가져온 카테고리를 select option에 하나씩 추가
+                                <option key={item.categoryId} value={item.categoryId}>{item.categoryTitle}</option>
+                            );
+                        })}
+                    </Form.Select>
+                </Form.Group>
 
-      <Form.Group className="mb-3">
-        <Form.Label>주소</Form.Label>
-        <Form.Control placeholder="주소를 입력하세요" />
-      </Form.Group>
+                <Form.Group className="mb-3">
+                    <Form.Label>주소</Form.Label>
+                    <Form.Control placeholder="주소를 입력하세요" />
+                </Form.Group>
 
-      <Form.Group className="mb-3">
-        <Form.Label>영업시간</Form.Label>
-        <Form.Control placeholder="영업시간을 입력하세요" />
-      </Form.Group>
+                <Form.Group className="mb-3">
+                    <Form.Label>영업시간</Form.Label>
+                    <Form.Control placeholder="영업시간을 입력하세요" />
+                </Form.Group>
 
-      <Form.Group className="mb-3">
-        <Form.Label>연락처</Form.Label>
-        <Form.Control placeholder="연락처를 입력하세요" />
-      </Form.Group>
+                <Form.Group className="mb-3">
+                    <Form.Label>연락처</Form.Label>
+                    <Form.Control placeholder="연락처를 입력하세요" />
+                </Form.Group>
 
-      <Form.Group className="mb-3">
-        <Form.Label>가게 소개</Form.Label>
-        <Form.Control as="textarea" rows={4} placeholder="소개글을 입력하세요" />
-      </Form.Group>
+                <Form.Group className="mb-3">
+                    <Form.Label>가게 소개</Form.Label>
+                    <Form.Control as="textarea" rows={4} placeholder="소개글을 입력하세요" />
+                </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="약관에 동의합니다" />
-      </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="약관에 동의합니다" />
+                </Form.Group>
 
-      <Button variant="primary" type="submit">
-        등록 요청
-      </Button>
-    </Form>
+                <Button variant="primary" type="submit">
+                    등록 요청
+                </Button>
+            </Form>
         </div>
     );
 };
