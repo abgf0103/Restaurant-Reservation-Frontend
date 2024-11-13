577b595e827f490b8d1a4950cc66332ba066c1ba
GiYoung <rlduddl5519@naver.com>
2024-11-13 11:27:18 +0900

    리뷰작성 링크 제거.
    ㅇㅇ

--- src/pages/user/Mypage.js
+++ src/pages/user/Mypage.js
@@ -10,9 +10,6 @@ const Mypage = () => {
         <Link to="/review/myreview">나의 리뷰 페이지</Link>
       </p>
       <p>
-        <Link to="/review">나의 리뷰 작성 페이지</Link>
-      </p>
-      <p>
         <Link to="/user/myreserve">나의 예약 페이지</Link>
       </p>
     </div>
