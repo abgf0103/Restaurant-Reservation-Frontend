1f682f1f0dd360bb6ba565d46c4a0c780dd64e17
Jong1 <abgf0103@gmail.com>
2024-11-18 11:10:53 +0900

    create : store edit

--- src/pages/store/StoreInfoEdit.js
+++ src/pages/store/StoreInfoEdit.js
@@ -11,27 +11,8 @@ import { useDaumPostcodePopup } from 'react-daum-postcode';
 const RegisterStore = () => {
     const navigate = useNavigate();
     const { storeId } = useParams(); // URL에서 storeId를 추출
-    // 카테고리 리스트를 저장하기 위한 state 선언
-    const [categoryList, setCategoryList] = useState([]);
-    const [address, setAddress] = useState('');
+    const [address, setAddress] = useState([]);
 
-    // 가게의 카테고리 정보를 가져오기
-    const [categoryInfo, setCategoryInfo] = useState([]);
-
-    const getCategoryInfo = () => {
-        instance.get()
-    }
-
-    // 카테고리 리스트를 API로 받아서 state에 저장
-    const getCategoryData = () => {
-        instance.get("/category/list").then((res) => {
-            setCategoryList(res.data);
-        });
-    };
-
-    useEffect(() => {
-        getCategoryData();
-    }, []);
 
     //유저정보 가져오기
     const [userInfo, setUserInfo] = useState('');
@@ -42,14 +23,18 @@ const RegisterStore = () => {
     }, []);
 
     // 가게 정보 가져오기
-    const [storeData, setStoreData] = useState([]);
+    const [storeData, setStoreData] = useState({
+        storeHours: '',
+        phone: '',
+        description: '',
+    });
+    console.log(storeData);
 
     const getStoreData = () =>{
         instance.get(`/store/view/${storeId}`).then((res) => {
             setStoreData(res.data);
         });
     };
-    console.log(storeData);
 
     useEffect(() => {
         getStoreData();
@@ -63,6 +48,7 @@ const RegisterStore = () => {
           ...prevState,
           [name]: value,
         }));
+
     }
 
     const [isAgrre, setIsAgree] = useState(false);
@@ -72,11 +58,6 @@ const RegisterStore = () => {
     }
 
 
-    const [storeCategory, setStoreCategory] = useState();
-
-    const storeCategoryHandler = (e) => {
-        setStoreCategory(e.target.value);
-    }
 
 
 
@@ -111,34 +92,6 @@ const RegisterStore = () => {
     //가게 수정 API 호출
     const requestStoreRegister = (e) => {
         e.preventDefault();
-
-        //가게 이름 체크 "/store/hasStoreName"
-        instance.get("/store/hasStoreName", {
-            params: {
-                storeName: storeData.storeName,
-            },
-        }).then((res) => {
-            if(res.data){
-                Swal.fire({
-                    title: "가게 이름 중복",
-                    text: "동일한 가게 이름이 존재합니다.",
-                    icon: "error",
-                });
-                return;
-            }
-        })
-
-        console.log(storeCategory);
-        console.log(storeData);
-
-        if(storeCategory === undefined){
-            Swal.fire({
-                title: "카테고리 선택",
-                text: "가게 수정을 진행하려면 카테고리를 선택하세요.",
-                icon: "warning",
-            });
-            return;
-        }
         
         if(!isAgrre) {
             Swal.fire({
@@ -149,10 +102,9 @@ const RegisterStore = () => {
             return;
         }
 
-        instance.post("/store/insert", {
-            userId: userInfo.id,
-            storeName: storeData.storeName,
-            address: address,
+        instance.post("/store/update", {
+            storeId: storeData.storeId,
+            address: storeData.address,
             storeHours: storeData.storeHours,
             phone: storeData.phone,
             description: storeData.description,
@@ -162,32 +114,7 @@ const RegisterStore = () => {
                 text: "가게 수정이 완료되었습니다.",
                 icon: "success",
             });
-            //storeName으로 storeId 찾아오기
-            console.log(storeData.storeName);
-            instance.get("/store/findStoreIdByStoreName", {
-                params: {
-                    storeName: storeData.storeName
-                }
-            }).then((res) => {
-                //찾아온 id로 storeCategory 수정
-                const storeId = res.data;
-                console.log(storeId);
-                instance.post("/storeCategory/save", {
-                    storeId: storeId,
-                    categoryId: storeCategory,
-                }).then(() => {
-                    navigate("/store/mystore"); // 내 가게 페이지로 이동
-                })
-                .catch((err) => {
-                    console.error("storeCategory 수정 오류:", err);
-                    Swal.fire({
-                        title: "카테고리 오류",
-                        text: "카테고리 수정에 오류가 발생했습니다. 나중에 다시 시도해주세요",
-                        icon: "error",
-                    });
-                    return;
-                });
-            })
+            navigate(`/store/mystore`);
           }).catch((error) => {
             console.error("가게 수정 오류:", error);
             Swal.fire({
@@ -203,24 +130,8 @@ const RegisterStore = () => {
             <h2>가게 수정 페이지</h2>
             <Form onSubmit={requestStoreRegister}>
                 <Form.Group className="mb-3">
-                    <Form.Label>가게 이름</Form.Label>
-                    <Form.Control type="text" placeholder="가게 이름을 입력하세요" value={storeData.storeName} onChange={onChangeHandler} required/>
-                    <Form.Text className="text-muted">
-                    We'll never share your email with anyone else.
-                    </Form.Text>
-                </Form.Group>
-
-                <Form.Group className="mb-3">
-                    <Form.Label>카테고리를 선택하세요</Form.Label>
-                    <Form.Select name='category' onChange={storeCategoryHandler} value={storeCategory} required>
-                        <option value="" hidden>카테고리를 선택하세요</option>
-                        {categoryList.map((item) => {
-                            return (       
-                                // categoryList 저장된 DB에서 가져온 카테고리를 select option에 하나씩 추가
-                                <option key={item.categoryId} value={item.categoryId}>{item.categoryTitle}</option>
-                            );
-                        })}
-                    </Form.Select>
+                    <Form.Label>{storeData.storeName}</Form.Label>
+                    
                 </Form.Group>
 
                 <Form.Group className="mb-3">
@@ -234,17 +145,17 @@ const RegisterStore = () => {
 
                 <Form.Group className="mb-3">
                     <Form.Label>영업시간 (ex/17:00~23:00)</Form.Label>
-                    <Form.Control placeholder="영업시간을 입력하세요" value={storeData.storeHours} onChange={onChangeHandler} required/>
+                    <Form.Control placeholder="영업시간을 입력하세요" name='storeHours' value={storeData.storeHours} onChange={onChangeHandler} required/>
                 </Form.Group>
 
                 <Form.Group className="mb-3">
                     <Form.Label>연락처</Form.Label>
-                    <Form.Control placeholder="연락처를 입력하세요" value={storeData.phone} onChange={onChangeHandler} required/>
+                    <Form.Control placeholder="연락처를 입력하세요" name='phone' value={storeData.phone} onChange={onChangeHandler} required/>
                 </Form.Group>
 
                 <Form.Group className="mb-3">
                     <Form.Label>가게 소개</Form.Label>
-                    <Form.Control as="textarea" rows={4} placeholder="소개글을 입력하세요" value={storeData.description} onChange={onChangeHandler} required/>
+                    <Form.Control as="textarea" rows={4} name='description' placeholder="소개글을 입력하세요" value={storeData.description} onChange={onChangeHandler} required/>
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicCheckbox">
