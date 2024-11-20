6b1eabbf83de3f82a25a1369841b838b4cf708ad
Jong1 <abgf0103@gmail.com>
2024-11-20 11:52:35 +0900

    fix : footer, category

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,21 +3,24 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import { useSelector } from "react-redux";
 import { useEffect } from 'react';
-import { useNavigate } from "react-router-dom";
+import { getUserInfo } from "../hooks/userSlice"
 import { useState } from 'react';
+import instance from './../api/instance';
 
 const Footer = () => {
+    const userInfo = useSelector(getUserInfo); // 로그인된 사용자 정보
+
+    const [isManager, setIsManager] = useState(false);
+
+    if (userInfo.id){
+        instance.get(`/user/isManagerByUserId?userId=${userInfo.id}`)
+        .then((res) =>{
+            if(res.data === 1){
+                setIsManager(true);
+            }
+        })
+    }
     
-    // const navigate = useNavigate();
-    // const userInfo = useSelector(getUserInfo); // 로그인된 사용자 정보
-  
-  
-    // 로그인 상태 체크
-    // useEffect(() => {
-    //   if (!userInfo.username) {
-    //     navigate("/user/login"); // 로그인 안 되어 있으면 로그인 페이지로 리다이렉트
-    //   }
-    // }, [navigate, userInfo]);
 
     return (
         <footer>
@@ -27,8 +30,8 @@ const Footer = () => {
                     <Nav.Link href="/">메인페이지</Nav.Link>
                     <Nav.Link href="/store/search">검색페이지</Nav.Link>
                     <Nav.Link href="/user/myreserve">나의 예약페이지</Nav.Link>
-                    <Nav.Link href="/store/mystore">나의 가게 페이지</Nav.Link>{" "}
-                    {/* 사업자 회원만 보이게 */}
+                    {/* 사업자 회원만 보이는 나의 가게 페이지*/}
+                    {isManager && <Nav.Link href="/store/mystore">나의 가게 페이지</Nav.Link>}
                     <Nav.Link href="/user/mypage">마이페이지</Nav.Link>
                 </Nav>
                 </Container>
--- src/pages/store/StoreList.js
+++ src/pages/store/StoreList.js
@@ -5,99 +5,94 @@ import { Link } from "react-router-dom";
 import SlideUpModal from "../../components/SlideUpModal";
 import "../../css/Style.css";
 
-const StoreList = () => {
-  // 가게 정보를 저장하기 위한 state 선언
-  const [storeData, setStoreData] = useState([]);
+    const StoreList = () => {
+    // 가게 정보를 저장하기 위한 state 선언
+    const [storeData, setStoreData] = useState([]);
 
-  // 예약 모달 상태 관리
-  const [isPanelOpen, setIsPanelOpen] = useState(false);
-  const [selectedStoreId, setSelectedStoreId] = useState(null); // 선택된 가게 ID
+    // 예약 모달 상태 관리
+    const [isPanelOpen, setIsPanelOpen] = useState(false);
+    const [selectedStoreId, setSelectedStoreId] = useState(null); // 선택된 가게 ID
 
-  // 가게 정보를 API로 받아서 state에 저장
-  const getData = () => {
-    instance.get("/store/list").then((res) => {
-      setStoreData(res.data);
-    });
-  };
-
-  useEffect(() => {
-    getData();
-  }, []);
-
-  // 예약 버튼 클릭 시 패널을 여는 함수
-  const handleReserveClick = (storeId) => {
-    setSelectedStoreId(storeId);
-    setIsPanelOpen(true);
-  };
-
-  const [categoryList, setCategoryList] = useState([]);
-
-  useEffect(() => {}, []);
-
-  useEffect(() => {
-    instance.get("/category/list").then((res) => {
-      console.log(res.data);
-      setCategoryList(res.data);
-    });
-  }, []);
-
-  const categoryClickHandler = (categoryId) => {
-    console.log(categoryId);
-    instance
-      .get(`/store/selectStoreByCategoryId?categoryId=${categoryId}`)
-      .then((res) => {
-        console.log(res.data);
+    // 가게 정보를 API로 받아서 state에 저장
+    const getData = () => {
+        instance.get("/store/list").then((res) => {
         setStoreData(res.data);
-      });
-  };
+        });
+    };
 
-  return (
-    <div>
-      <h4>카테고리</h4>
-      <button>전체</button>
-      {categoryList.map((item) => (
-        <button
-          key={item.categoryId}
-          onClick={() => categoryClickHandler(item.categoryId)}
-        >
-          {item.categoryTitle}
-        </button>
-      ))}
-      <h4>==========가게 정보 리스트==========</h4>
-      <ul>
-        {storeData.map((item) => {
-          return (
-            <li key={item.storeId}>
-              <Card style={{ width: "18rem" }}>
-                <Link to={"/store/info"} state={item.storeId}>
-                  <Card.Img variant="top" src="holder.js/100px180" />
-                  <Card.Body>
-                    <Card.Title>{item.storeName}</Card.Title>
-                    <Card.Text>{item.description}</Card.Text>
-                  </Card.Body>
-                </Link>
-                <button
-                  className="reserve-button-list"
-                  onClick={() => handleReserveClick(item.storeId)}
-                  style={{ marginTop: "10px" }}
-                >
-                  예약하기
-                </button>
-              </Card>
-            </li>
-          );
-        })}
-      </ul>
-      <h4>===============================</h4>
+    useEffect(() => {
+        getData();
+    }, []);
 
-      {/* 슬라이드 업 예약 폼 모달 */}
-      <SlideUpModal
-        isOpen={isPanelOpen}
-        onClose={() => setIsPanelOpen(false)}
-        selectedStoreId={selectedStoreId} // 올바르게 selectedStoreId 전달
-      />
-    </div>
-  );
-};
+    // 예약 버튼 클릭 시 패널을 여는 함수
+    const handleReserveClick = (storeId) => {
+        setSelectedStoreId(storeId);
+        setIsPanelOpen(true);
+    };
 
-export default StoreList;
+    const [categoryList, setCategoryList] = useState([]);
+
+    useEffect(() => {
+        instance.get("/category/list").then((res) => {
+        setCategoryList(res.data);
+        });
+    }, []);
+
+    const categoryClickHandler = (categoryId) => {
+        instance
+        .get(`/store/selectStoreByCategoryId?categoryId=${categoryId}`)
+        .then((res) => {
+            setStoreData(res.data);
+        });
+    };
+
+    return (
+        <div>
+        <h4>카테고리</h4>
+        <button onClick={() => getData()}>전체</button>
+        {categoryList.map((item) => (
+            <button
+            key={item.categoryId}
+            onClick={() => categoryClickHandler(item.categoryId)}
+            >
+            {item.categoryTitle}
+            </button>
+        ))}
+        <h4>==========가게 정보 리스트==========</h4>
+        <ul>
+            {storeData.map((item) => {
+            return (
+                <li key={item.storeId}>
+                <Card style={{ width: "18rem" }}>
+                    <Link to={"/store/info"} state={item.storeId}>
+                    <Card.Img variant="top" src="holder.js/100px180" />
+                    <Card.Body>
+                        <Card.Title>{item.storeName}</Card.Title>
+                        <Card.Text>{item.description}</Card.Text>
+                    </Card.Body>
+                    </Link>
+                    <button
+                    className="reserve-button-list"
+                    onClick={() => handleReserveClick(item.storeId)}
+                    style={{ marginTop: "10px" }}
+                    >
+                    예약하기
+                    </button>
+                </Card>
+                </li>
+            );
+            })}
+        </ul>
+        <h4>===============================</h4>
+
+        {/* 슬라이드 업 예약 폼 모달 */}
+        <SlideUpModal
+            isOpen={isPanelOpen}
+            onClose={() => setIsPanelOpen(false)}
+            selectedStoreId={selectedStoreId} // 올바르게 selectedStoreId 전달
+        />
+        </div>
+    );
+    };
+
+    export default StoreList;
