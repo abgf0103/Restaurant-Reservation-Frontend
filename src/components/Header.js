c8879e34063fa0b59916b2f0649192a6e6ad1d86
Jong1 <abgf0103@gmail.com>
2024-12-06 09:49:02 +0900

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,189 +15,185 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleUser } from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
-  const navigate = useNavigate();
-  const userInfo = useSelector(getUserInfo); // Redux에서 로그인된 사용자 정보 가져오기
-  const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const userInfo = useSelector(getUserInfo); // Redux에서 로그인된 사용자 정보 가져오기
+    const dispatch = useDispatch();
 
-  const [isMobileView, setIsMobileView] = useState(false); // 모바일 뷰 판단을 위한 상태
+    const [isMobileView, setIsMobileView] = useState(false); // 모바일 뷰 판단을 위한 상태
 
-  // 화면 크기에 따라 모바일 뷰 상태 업데이트
-  useEffect(() => {
-    const handleResize = () => {
-      setIsMobileView(window.innerWidth < 994); // 994 이하일 때 드롭다운 버튼 생성
+    // 화면 크기에 따라 모바일 뷰 상태 업데이트
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobileView(window.innerWidth < 994); // 994 이하일 때 드롭다운 버튼 생성
+        };
+
+        // 초기 설정 및 resize 이벤트 리스너 추가
+        handleResize();
+        window.addEventListener("resize", handleResize);
+
+        // 컴포넌트 언마운트 시 이벤트 리스너 제거
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
+
+    // 로그아웃 처리
+    const handleLogout = () => {
+        localStorage.removeItem("tokenInfo");
+        localStorage.removeItem("userInfo");
+        dispatch(removeUserInfo());
+        dispatch(removeTokenInfo());
+        navigate("/");
     };
 
-    // 초기 설정 및 resize 이벤트 리스너 추가
-    handleResize();
-    window.addEventListener("resize", handleResize);
-
-    // 컴포넌트 언마운트 시 이벤트 리스너 제거
-    return () => {
-      window.removeEventListener("resize", handleResize);
+    const goBack = () => {
+        navigate(-1);
     };
-  }, []);
 
-  // 로그아웃 처리
-  const handleLogout = () => {
-    localStorage.removeItem("tokenInfo");
-    localStorage.removeItem("userInfo");
-    dispatch(removeUserInfo());
-    dispatch(removeTokenInfo());
-    navigate("/");
-  };
+    const [searchKeyword, setSearchKeyword] = useState([]);
 
-  const goBack = () => {
-    navigate(-1);
-  };
+    const handleSearchChange = (e) => {
+        setSearchKeyword(e.target.value);
+    };
 
-  const [searchKeyword, setSearchKeyword] = useState([]);
+    const searchStore = (keyword) => {
+        instance.get(`/store/search?searchKeyword=${keyword}`).then((res) => {
+            const result = res.data;
+            navigate("/", { state: { result } });
+        });
+    };
 
-  const handleSearchChange = (e) => {
-    setSearchKeyword(e.target.value);
-  };
+    const handleSearchSubmit = (e) => {
+        e.preventDefault();
+        searchStore(searchKeyword);
+    };
 
-  const searchStore = (keyword) => {
-    instance.get(`/store/search?searchKeyword=${keyword}`).then((res) => {
-      const result = res.data;
-      navigate("/", { state: { result } });
-    });
-  };
+    console.log(userInfo);
 
-  const handleSearchSubmit = (e) => {
-    e.preventDefault();
-    searchStore(searchKeyword);
-  };
-
-  return (
-    <header>
-      <Navbar expand="lg">
-        <Container id="headerContainer">
-          {window.location.pathname === "/" ? null : (
-            <Button id="back" onClick={goBack}>
-              ←
-            </Button>
-          )}
-          <Navbar.Brand href="/">
-            <img src={logoImg} alt="Logo" className="logoImg" />
-          </Navbar.Brand>
-          <Form onSubmit={handleSearchSubmit} id="search-form">
-            <div id="search-bar">
-              <FormControl
-                type="search"
-                placeholder="검색어를 입력하세요"
-                className="mr-sm-2"
-                value={searchKeyword}
-                onChange={handleSearchChange}
-              />
-            </div>
-            <Button id="search" type="submit">
-              검색
-            </Button>
-          </Form>
-          {isMobileView ? (
-            <Dropdown>
-              <Dropdown.Toggle id="dropdown-basic"></Dropdown.Toggle>
-              <Dropdown.Menu id="dropdown-menu">
-                {userInfo && userInfo.username ? (
-                  <Dropdown.Item id="dropdown-item">
-                    <div className="btn-container">
-                      <span className="nickname">
-                        {userInfo.username + "님"}{" "}
-                      </span>
-                      <Button onClick={handleLogout} id="logout">
-                        로그아웃
-                      </Button>
-                    </div>
-                  </Dropdown.Item>
-                ) : (
-                  <div className="btn-container">
-                    <Dropdown.Item id="dropdown-item">
-                      <Button
-                        id="login"
-                        onClick={() => navigate("/user/login")}
-                      >
-                        로그인
-                      </Button>
-                    </Dropdown.Item>
-                    <Dropdown.Item id="dropdown-item">
-                      <Button
-                        id="join"
-                        onClick={() => navigate("/user/PreUserEdit")}
-                      >
-                        회원가입
-                      </Button>
-                    </Dropdown.Item>
-                  </div>
-                )}
-              </Dropdown.Menu>
-            </Dropdown>
-          ) : (
-            <div id="user-buttons">
-              {userInfo && userInfo.username ? (
-                <>
-                  {userInfo.fileId ? (
-                    <Link
-                      to="/user/mypage"
-                      style={{ width: "40px", height: "40px" }}
-                    >
-                      {/* 프로필 사진이 있을 경우 */}
-                      <div
-                        style={{
-                          width: "40px",
-                          height: "40px",
-                          borderRadius: "50%",
-                          border: "1.5px solid var(--secondary-color)",
-                          overflow: "hidden",
-                          backgroundImage: `url(${process.env.REACT_APP_HOST}/file/viewId/${userInfo.fileId})`,
-                          backgroundPosition: "center",
-                          backgroundSize: "cover",
-                        }}
-                      ></div>
-                    </Link>
-                  ) : (
-                    // 프로필 사진이 없으면 기본 아이콘 표시
-                    <Link
-                      to="/user/mypage"
-                      style={{ width: "40px", height: "40px" }}
-                    >
-                      <FontAwesomeIcon
-                        className="mypage-default-icon"
-                        icon={faCircleUser}
-                        style={{
-                          width: "40px",
-                          height: "40px",
-                          borderRadius: "50%",
-                          border: "1.5px solid var(--secondary-color)",
-                          color: "#212529",
-                        }}
-                      />
-                    </Link>
-                  )}
-                  <span className="nickname">{userInfo.username + "님"} </span>
-                  <Button onClick={handleLogout} id="logout">
-                    로그아웃
-                  </Button>
-                </>
-              ) : (
-                <>
-                  <Button id="login" onClick={() => navigate("/user/login")}>
-                    로그인
-                  </Button>
-                  <Button
-                    id="join"
-                    onClick={() => navigate("/user/PreUserEdit")}
-                  >
-                    회원가입
-                  </Button>
-                </>
-              )}
-            </div>
-          )}
-          <Notification />
-        </Container>
-      </Navbar>
-    </header>
-  );
+    return (
+        <header>
+            <Navbar expand="lg">
+                <Container id="headerContainer">
+                    {window.location.pathname === "/" ? null : (
+                        <Button id="back" onClick={goBack}>
+                            ←
+                        </Button>
+                    )}
+                    <Navbar.Brand href="/">
+                        <img src={logoImg} alt="Logo" className="logoImg" />
+                    </Navbar.Brand>
+                    <Form onSubmit={handleSearchSubmit} id="search-form">
+                        <div id="search-bar">
+                            <FormControl
+                                type="search"
+                                placeholder="검색어를 입력하세요"
+                                className="mr-sm-2"
+                                value={searchKeyword}
+                                onChange={handleSearchChange}
+                            />
+                        </div>
+                        <Button id="search" type="submit">
+                            검색
+                        </Button>
+                    </Form>
+                    {isMobileView ? (
+                        <Dropdown>
+                            <Dropdown.Toggle id="dropdown-basic"></Dropdown.Toggle>
+                            <Dropdown.Menu id="dropdown-menu">
+                                {userInfo && userInfo.username ? (
+                                    <Dropdown.Item id="dropdown-item">
+                                        <div className="btn-container">
+                                            <div
+                                                style={{
+                                                    width: "40px",
+                                                    height: "40px",
+                                                    borderRadius: "50%",
+                                                    border: "1.5px solid var(--secondary-color)",
+                                                    overflow: "hidden",
+                                                    backgroundImage: `url(${process.env.REACT_APP_HOST}/file/viewId/${userInfo.fileId})`,
+                                                    backgroundPosition: "center",
+                                                    backgroundSize: "cover",
+                                                }}
+                                            ></div>
+                                            <Button onClick={handleLogout} id="logout">
+                                                로그아웃
+                                            </Button>
+                                        </div>
+                                    </Dropdown.Item>
+                                ) : (
+                                    <div className="btn-container">
+                                        <Dropdown.Item id="dropdown-item">
+                                            <Button id="login" onClick={() => navigate("/user/login")}>
+                                                로그인
+                                            </Button>
+                                        </Dropdown.Item>
+                                        <Dropdown.Item id="dropdown-item">
+                                            <Button id="join" onClick={() => navigate("/user/PreUserEdit")}>
+                                                회원가입
+                                            </Button>
+                                        </Dropdown.Item>
+                                    </div>
+                                )}
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    ) : (
+                        <div id="user-buttons">
+                            {userInfo && userInfo.username ? (
+                                <>
+                                    {userInfo.fileId ? (
+                                        <Link to="/user/mypage" style={{ width: "40px", height: "40px" }}>
+                                            {/* 프로필 사진이 있을 경우 */}
+                                            <div
+                                                style={{
+                                                    width: "40px",
+                                                    height: "40px",
+                                                    borderRadius: "50%",
+                                                    border: "1.5px solid var(--secondary-color)",
+                                                    overflow: "hidden",
+                                                    backgroundImage: `url(${process.env.REACT_APP_HOST}/file/viewId/${userInfo.fileId})`,
+                                                    backgroundPosition: "center",
+                                                    backgroundSize: "cover",
+                                                }}
+                                            ></div>
+                                        </Link>
+                                    ) : (
+                                        // 프로필 사진이 없으면 기본 아이콘 표시
+                                        <Link to="/user/mypage" style={{ width: "40px", height: "40px" }}>
+                                            <FontAwesomeIcon
+                                                className="mypage-default-icon"
+                                                icon={faCircleUser}
+                                                style={{
+                                                    width: "40px",
+                                                    height: "40px",
+                                                    borderRadius: "50%",
+                                                    border: "1.5px solid var(--secondary-color)",
+                                                    color: "#212529",
+                                                }}
+                                            />
+                                        </Link>
+                                    )}
+                                    <span className="nickname">{userInfo.name + "님"} </span>
+                                    <Button onClick={handleLogout} id="logout">
+                                        로그아웃
+                                    </Button>
+                                </>
+                            ) : (
+                                <>
+                                    <Button id="login" onClick={() => navigate("/user/login")}>
+                                        로그인
+                                    </Button>
+                                    <Button id="join" onClick={() => navigate("/user/PreUserEdit")}>
+                                        회원가입
+                                    </Button>
+                                </>
+                            )}
+                        </div>
+                    )}
+                    <Notification />
+                </Container>
+            </Navbar>
+        </header>
+    );
 };
 
 export default Header;
